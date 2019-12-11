66368e6abb325cdf74c9cb537fe5703cc1b69224
Keelan Robinson <keelan@kesiro.uk>
2019-12-11 16:24:32 +0000

    Go to registration
    Go to registration

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const Nightmare = require('nightmare')
-const nightmare = Nightmare({ show: true })
+const nightmare = Nightmare({ show: true, height:768, width:1366})
 
 nightmare
   .goto('https://apps.fliplet.com/ucl-agx')
   .click('.swiper-button-next-2462079')
   .click('input[value="Login "]')
+  .goto('https://apps.fliplet.com/v1/apps/11449/pages/173888/view')
   .wait(5000)
   .end()
   .then(console.log)
